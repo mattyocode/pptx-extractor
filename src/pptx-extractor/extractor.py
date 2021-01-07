c1f0ca92b3d8ed00d48b889de450343f028c6c41
mattyocode <matthewoliver@live.co.uk>
2021-01-07 16:20:32 +0000

    Feat: save images to directory

--- src/pptx-extractor/extractor.py
+++ src/pptx-extractor/extractor.py
@@ -1,15 +1,36 @@
-from pptx import Presentation
 import os
+import re
 
+from pptx import Presentation
+from pptx.enum.shapes import MSO_SHAPE_TYPE
+
+
+def write_image(shape, slide_id, shape_no, deck_name):
+    image = shape.image
+    image_bytes = image.blob
+    image_filename = f'{slide_id}{shape_no}.{image.ext}'
+    with open(f'images/{deck_name}/{image_filename}', 'wb') as f:
+        f.write(image_bytes)
 
 
 def extract_text(file_path):
     prs = Presentation(file_path)
+    deck_name = re.findall(r'input_files/(.*?).pptx', file_path)[0] 
+    os.mkdir(f'images/{deck_name}')
     text_list = []
     for slide in prs.slides:
+        slide_id = slide.slide_id
+        slide_list = []
         for shape in slide.shapes:
+            shape_no = 0
             if hasattr(shape, 'text'):
-                text_list.append(shape.text)
+                if shape.text != '':
+                    slide_list.append(shape.text)
+            if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
+                write_image(shape, slide_id, shape_no, deck_name)
+            shape_no += 1
+        text_list.append(slide_list)
+
     return text_list
 
 
